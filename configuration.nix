1c0bce66c444915b639f31cc5729dfd5dd2555ea
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-06 18:45:09 +0100

    stuffs

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
     ];
@@ -22,19 +22,19 @@
 
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.defaultLocale = "en_DK.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
+#  i18n.extraLocaleSettings = {
+#    LC_ADDRESS = "de_DE.UTF-8";
+#    LC_IDENTIFICATION = "de_DE.UTF-8";
+#    LC_MEASUREMENT = "de_DE.UTF-8";
+#    LC_MONETARY = "de_DE.UTF-8";
+#    LC_NAME = "de_DE.UTF-8";
+#    LC_NUMERIC = "de_DE.UTF-8";
+#    LC_PAPER = "de_DE.UTF-8";
+#    LC_TELEPHONE = "de_DE.UTF-8";
+#    LC_TIME = "en_DK.UTF-8";
+#  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -55,10 +55,8 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
   hardware.bluetooth.enable = true;
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -67,8 +65,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
+    
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -86,15 +84,7 @@
     description = "xzvf";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "dialout" "networkmanager" ];
-    packages = with pkgs; [
-      firefox
-      kate
-      thunderbird
-      alacritty
-      kitty
-      #fira-code-nerdfont
-      fastfetch
-    ];
+    packages = with pkgs; [ ];
   };
 
   home-manager = {
@@ -110,41 +100,28 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
      neovim
+     wget
      git
      gcc
      zsh
      stow
+     htop
+     btop
+     ripgrep
+     ripgrep-all
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
+  programs.mtr.enable = true;
   programs.gnupg.agent = {
      enable = true;
      enableSSHSupport = true;
   };
 
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05";
 
 }
--- home.nix
+++ home.nix
@@ -3,20 +3,18 @@
 {
   home.username = "xzvf";
   home.homeDirectory = "/home/xzvf";
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  #home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "FiraCodeMono" ]; })
+  home.packages = with pkgs; [
+    clang
+    firefox
+    kate
+    thunderbird
+    alacritty
+    kitty
+    fastfetch
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -40,26 +38,11 @@
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/xzvf/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    BROWSER = "firefox";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
