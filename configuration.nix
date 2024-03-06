7eb79e9a9420e24a9bed3011be9f93a44a86cefe
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-06 22:56:02 +0100

    Basic configuration done

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,21 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+     neovim
+     wget
+     git
+     gcc
+     zsh
+     stow
+     htop
+     btop
+     ripgrep
+     ripgrep-all
+     rsync
+     easyeffects
+  ];
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   imports =
     [
@@ -17,12 +28,16 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "rubidium"; # Define your hostname.
   networking.networkmanager.enable = true;
+  
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
+  services.resolved.enable = true;
 
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
 #  i18n.extraLocaleSettings = {
 #    LC_ADDRESS = "de_DE.UTF-8";
@@ -36,24 +51,16 @@
 #    LC_TIME = "en_DK.UTF-8";
 #  };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
     variant = "";
   };
-
-  # Configure console keymap
   console.keyMap = "uk";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
   hardware.bluetooth.enable = true;
 
@@ -66,25 +73,17 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.fprintd.enable = true;
 
   programs.zsh.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.xzvf = {
     isNormalUser = true;
     description = "xzvf";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "dialout" "networkmanager" ];
-    packages = with pkgs; [ ];
   };
 
   home-manager = {
@@ -94,34 +93,23 @@
     };
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     neovim
-     wget
-     git
-     gcc
-     zsh
-     stow
-     htop
-     btop
-     ripgrep
-     ripgrep-all
-  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.mtr.enable = true;
   programs.gnupg.agent = {
      enable = true;
      enableSSHSupport = true;
   };
 
-  # services.openssh.enable = true;
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 * * * *      xzvf    . /etc/profile; pferd"
+    ];
+  };
+
+
   networking.firewall.enable = false;
   system.stateVersion = "24.05";
-
 }
--- home.nix
+++ home.nix
@@ -6,7 +6,15 @@
   #home.stateVersion = "23.11"; # Please read the comment before changing.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
   home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
     clang
     firefox
     kate
@@ -14,18 +22,17 @@
     alacritty
     kitty
     fastfetch
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    bitwarden
+    jetbrains.idea-ultimate
+    jetbrains.datagrip
+    nextcloud-client
+    mpv
+    vlc
+    pferd
+    rambox
+    homebank
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -42,6 +49,13 @@
     EDITOR = "nvim";
     VISUAL = "nvim";
     BROWSER = "firefox";
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+    ];
   };
 
   programs.home-manager.enable = true;
