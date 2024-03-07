c72418682b0f2da882216d82bfd4812e9604de45
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-07 19:53:27 +0100

    Added tmux-powerline plugin

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -1,19 +1,30 @@
 {config, pkgs, ... }:
+let
+ tmuxPowerline = 
+        pkgs.tmuxPlugins.mkTmuxPlugin rec {
+          pluginName = "tmux-powerline";
+          version = "3.0.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "erikw";
+            repo = "tmux-powerline";
+            rev = "v${version}";
+	    sha256 = "25uG7OI8OHkdZ3GrTxG1ETNeDtW1K+sHu2DfJtVHVbk=";
+          };
+	  rtpFilePath = "main.tmux";
+        };
+in
 {
-
-  programs.tmux = {
-    enable = true;
-    historyLimit = 10000;
-    keyMode = "vi";
-    mouse = true;
-    extraConfig = builtins.readFile ./tmux.conf;
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      #mkTmuxPlugin {
-      #  pluginName = "tmux-powerline";
-      #  version 
-      #};
-    ];
+  config = {
+    programs.tmux = {
+      enable = true;
+      historyLimit = 10000;
+      keyMode = "vi";
+      mouse = true;
+      extraConfig = builtins.readFile ./tmux.conf;
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+	tmuxPowerline
+      ];
+    };
   };
-
 }
