25dc161b2f9afa5f47cad374908d33e1bdcfe1de
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-08 11:05:10 +0100

    Better bluetooth config

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,24 @@
   services.xserver.desktopManager.plasma6.enable = true;
 
   hardware.enableAllFirmware = true;
-  hardware.bluetooth.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+	Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+
+
   services.hardware.bolt.enable = true;
   services.printing.enable = true;
   services.fprintd.enable = true;
