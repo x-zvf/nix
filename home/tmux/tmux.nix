e165a3e8487449c0f41c3de72a0f99fccfc03355
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-08 11:07:16 +0100

    tmux: Dracula powerline config and fix split commands

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -9,7 +9,23 @@
       extraConfig = builtins.readFile ./tmux.conf;
       plugins = with pkgs.tmuxPlugins; [
         sensible
-	catppuccin
+	#catppuccin
+	{
+	  plugin = dracula;
+	  extraConfig = ''
+set -g @dracula-show-flags true
+set -g @dracula-show-powerline true
+set -g @dracula-show-fahrenheit false
+set -g @dracula-git-show-remote-status true
+set -g @dracula-plugins "battery git cpu-usage time"
+set -g @dracula-show-left-sep 
+set -g @dracula-show-right-sep 
+set -g @dracula-battery-label "  "
+set -g @dracula-show-left-icon " "
+set -g @dracula-time-format "%F %H:%M:%S"
+set -g @dracula-refresh-rate 1
+'';
+	}
 	tmux-thumbs
 	tmux-fzf
       ];
