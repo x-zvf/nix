72b644d9074c4036f0f83719b8597f4db79bbf4b
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-08 21:16:11 +0100

    Enable dconf

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
+  programs.dconf.enable = true;
 
   hardware.enableAllFirmware = true;
 
