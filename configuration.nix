3974062cbc28eb15e81496d4ab44228c812ab218
Péter Bohner (xzvf) <peter@bohner.me>
2024-03-14 09:53:13 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
   programs.dconf.enable = true;
 
   hardware.enableAllFirmware = true;
