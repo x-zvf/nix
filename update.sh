7798f065627f57d240278ef10cf78aa18cc2bbee
Péter Bohner (xzvf) <peter@bohner.me>
2024-06-09 15:49:34 +0200

    update.sh: commit as last step

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -xe
 cd "$(dirname "$0")"
 sudo echo "Caching sudo"
 nix flake update
-git add ./flake.lock
-git commit -m "update flake"
 home-manager --flake '/home/xzvf/nix#xzvf' switch
 sudo nixos-rebuild switch --flake '/home/xzvf/nix#default'
+git add ./flake.lock
+git commit -m "update flake"
