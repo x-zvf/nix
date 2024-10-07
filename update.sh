a400563011265b155a29bd8bc229c84d5528ab8d
Péter Bohner (xzvf) <peter@bohner.me>
2024-10-07 08:43:30 +0200

    simplify update script

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ set -xe
 
 cd "$(dirname "$0")"
 nix flake update
-#home-manager --flake '/home/xzvf/nix#xzvf' switch
 sudo nixos-rebuild --flake '/home/xzvf/nix#default' switch
 git add ./flake.lock
 git commit -m "update flake"
