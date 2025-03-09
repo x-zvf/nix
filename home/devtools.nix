42bd99b074ff5ef1f187097b7c01dc8ff0ec8f3e
Péter Bohner (xzvf) <peter@bohner.me>
2025-03-09 16:01:39 +0100

    update programs.vscode

--- home/devtools.nix
+++ home/devtools.nix
@@ -85,7 +85,7 @@
   ];
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
     ];
   };
