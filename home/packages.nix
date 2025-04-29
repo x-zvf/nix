017ef0b74ea8bdd21127a15432e60cf3ff9fc8f0
Péter Bohner (xzvf) <peter@bohner.me>
2025-04-29 10:00:21 +0200

    fix pympress

--- home/packages.nix
+++ home/packages.nix
@@ -34,6 +34,16 @@ in {
     qalculate-qt
     nextcloud-client
     #pympress
+    (pympress.overrideAttrs (prev: {
+      version = "1.8.5+e5da914";
+      src = fetchFromGitHub {
+        owner = "Cimbali";
+        repo = "pympress";
+        rev = "e5da914ecb2f5beb6298225cde510e5ff07d02bc";
+        sha256 = "sha256-9n/xfKwN85zqP/5wGjkRbjYo4q40yJE1ABmi40Fz9dk=";
+      };
+      buildInputs = prev.buildInputs ++ [python3Packages.babel];
+    }))
     signal-desktop-bin
     ferdium
     tor-browser
