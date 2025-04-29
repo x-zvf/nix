538af3d9a972b7afed7e9652fe51e515e2119cee
Péter Bohner (xzvf) <peter@bohner.me>
2025-04-29 10:21:30 +0200

    neovim: start with all folds open

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -27,6 +27,7 @@ vim.opt.expandtab = true
 -- folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldenable = false
 
 -- keybindings
 vim.g.mapleader = " "
