20ce0554832a48bdeeb78d5fd654f5c60933ef20
Péter Bohner (xzvf) <peter@bohner.me>
2025-06-23 23:20:33 +0200

    rubidium: backups

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,15 +1,9 @@
 {
-  config,
   lib,
   pkgs,
-  modulesPath,
   ...
 }:
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
@@ -33,6 +27,14 @@
       }
     ];
   };
+
+  environment.systemPackages = with pkgs; [
+    zfs-autobackup
+  ];
+
+  services.cron.systemCronJobs = [
+    "10 * * * *     root    . /etc/profile; zfs-autobackup -v --ssh-target gabackup --ssh-config /home/xzvf/.ssh/config togallium RustBucket/backups/rubidium"
+  ];
 
   fileSystems."/" = {
     device = "zpool/root";
@@ -65,16 +67,11 @@
 
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  #  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
-
   systemd.network.enable = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.cpu.amd.updateMicrocode = true;
+  hardware.enableAllFirmware = true;
+
   hardware.amdgpu.opencl.enable = true;
 }
