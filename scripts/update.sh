9aa4466274f131aea66fcda17f4e4d57a52bd56c
Péter Bohner (xzvf) <peter@bohner.me>
2025-08-01 18:07:53 +0200

    update script works on multiple hosts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -xe
 
-cd "$(dirname "$0")"
+#cd "$(dirname "$0")"
+cd /home/xzvf/nix
 nix flake update
-sudo nixos-rebuild --flake '/home/xzvf/nix#default' switch
+sudo nixos-rebuild --flake "/home/xzvf/nix#$(hostname)" switch
 git add ./flake.lock
 git commit -m "update flake"
